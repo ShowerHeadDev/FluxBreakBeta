70ccfee3adc3f247104279b503443669d1bc5db6
ShowerHeadDev <58641221+ShowerHeadDev@users.noreply.github.com>
2019-12-08 14:37:51 -0800

    Update bruh.lua

--- bruh.lua
+++ bruh.lua
@@ -1,1 +1,5 @@
+if XPROTECT == nil then
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/bruh2.txt"), true))() 
+else 
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/SirhurtVersion.txt"), true))() 
+end
