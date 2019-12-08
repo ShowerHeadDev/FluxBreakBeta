80df5d4c128beb3b79bd869d48c7ee0fe90fa52d
ShowerHeadDev <58641221+ShowerHeadDev@users.noreply.github.com>
2019-12-08 14:54:06 -0800

    Update bruh.lua

--- bruh.lua
+++ bruh.lua
@@ -1,5 +1,7 @@
-if XPROTECT == nil then
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/bruh2.txt"), true))() 
+if rconsoleerr == nil then
+  if XPROTECT ~= nil then
+       loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/SirhurtVersion.txt"), true))() 
+   end
 else 
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/SirhurtVersion.txt"), true))() 
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/bruh2.txt"), true))() 
 end
