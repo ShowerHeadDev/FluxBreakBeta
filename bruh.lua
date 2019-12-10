65fd4269e11ccc393f5a3e28fda1ab8f2d2bd08b
ShowerHeadDev <58641221+ShowerHeadDev@users.noreply.github.com>
2019-12-09 18:18:55 -0800

    Update bruh.lua

--- bruh.lua
+++ bruh.lua
@@ -1,6 +1,19 @@
 if rconsoleerr == nil then
   if XPROTECT ~= nil then
        loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/SirhurtVersion.txt"), true))() 
+   else if printoutput ~= nil then
+      if PROTOSMASHER_LOADED and (not shared.repair) then
+    warn("Loading ProtoSmasher Fixes, Ty wally and Classy")--credits to wally
+    coroutine.wrap(function()
+        pcall(function()
+            loadstring(game:HttpGet("https://pastebin.com/raw/m8zyuQMv", true))()
+        end)
+    end)()
+    repeat wait() until shared.repair
+end
+
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/ProtoSmasher.txt"), true))() 
+      end
    end
 else 
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/ShowerHeadDev/FluxBreakBeta/master/bruh2.txt"), true))() 
